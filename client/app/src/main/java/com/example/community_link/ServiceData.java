1b177b78c0c2697b2f9186ea7fcda8b017dd9188
EastEden <alexis10cap@gmail.com>
2020-10-19 14:30:24 -0700

    Updated ServiceData for more accurate JSON

--- client/app/src/main/java/com/example/community_link/ServiceData.java
+++ client/app/src/main/java/com/example/community_link/ServiceData.java
@@ -8,28 +8,27 @@ import com.google.gson.*;
 
 @SuppressWarnings("serial") //With this annotation we are going to hide compiler warnings
 public class ServiceData implements Serializable{
-    private String userName;
-    private int year;
-    private int month;
-    private int day;
-    private int hour;
-    private int minute;
-    private String eventName;
+    private String owner;
+    private String date;
+    private String time;
+    private String name;
     private String dow;
     private String type;
     private String description;
+    private double longi;
+    private double lat;
 
     public ServiceData(){
+        longi = -123.116226;
+        lat = 49.246292;
     }
 
     public void setName(String name){
-        this.userName = name;
+        this.owner = name;
     }
 
     public void setDate(int year, int month, int day){
-        this.year = year;
-        this.month = month;
-        this.day = day;
+        this.date = year+"-"+month+"-"+day;
         SimpleDateFormat simpledateformat = new SimpleDateFormat("EEEE");
         Date date = new Date(year, month, day-1);
         String dow = simpledateformat.format(date);
@@ -37,12 +36,11 @@ public class ServiceData implements Serializable{
     }
 
     public void setTime(int hour, int minute){
-        this.hour = hour;
-        this.minute = minute;
+        this.time = hour+":"+minute;
     }
 
     public void setEventName(String eventName){
-        this.eventName = eventName;
+        this.name = eventName;
     }
 
     public void setType(String type){

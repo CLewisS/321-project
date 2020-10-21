86ffc836500a835ce682acef6c51e575164c40ba
CLewisS <clsattler11@gmail.com>
2020-10-20 21:38:36 -0700

    Started to implement push notifications

--- client/app/src/main/java/com/example/community_link/MainActivity.java
+++ client/app/src/main/java/com/example/community_link/MainActivity.java
@@ -1,19 +1,55 @@
 package com.example.community_link;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId("APP ID") // Required for Analytics.
+                .setProjectId("PROJECT ID") // Required for Firebase Installations.
+                .setApiKey("GOOGLE API KEY") // Required for Auth.
+                .build();
+        FirebaseApp.initializeApp(this, options, "Community_Link");
+
+        System.out.println("Getting instance id");
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult() + " ------------------------------------------";
+
+                        // Log and toast
+                        Log.d(TAG, token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public void addService(View view) {
---
+++ client/app/src/main/java/com/example/community_link/MyFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package com.example.community_link;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    /**
+     * Called if FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve
+     * the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        //sendRegistrationToServer(token);
+    }
+}

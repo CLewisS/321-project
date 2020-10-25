3347f174f776b3576578abb5911fb13cec3f6dea
Brendonchenhao <suneduxch@gmail.com>
2020-10-25 10:49:14 -0700

    checkforcharserver

---
+++ server/chatServer/chatCheck.js
@@ -0,0 +1,36 @@
+
+messageGetAttribute = ["user1", "user2", "newest"];
+messageAttribute = ["sender", "recipient", "timestamp", "content"];
+
+
+/* Check if the get message query is valid.
+ * Parameters:
+ *   - query: a json object get from the get message request query.
+ */
+module.exports.checkMessageQuery= function(query) {
+    const keys = Object.keys(query);
+    for (var key of keys){
+        if( !messageGetAttribute.includes(key) ){
+            throw key + " is not a valid key";
+        }
+    }
+}
+
+
+
+/* Check if the add message is valid.
+ * Parameters:
+ *   - message: a json object represents the message.
+ */
+module.exports.checkMessage= function(message) {
+    const keys = Object.keys(message);
+    for (var key of keys){
+        if( !messageAttribute.includes(key) ){
+            throw key + " is not a valid key";
+        }
+    }
+
+    if (typeof(message.content) !== "string"){
+        throw "Message should be a string, not a " + typeof(message.content) + "."
+    }
+}
--- server/chatServer/chatServer.js
+++ server/chatServer/chatServer.js
@@ -5,10 +5,10 @@ var db = require("../dbInterface/chatDB.js");
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./fcm-key.json");
-
+var check = require("./chatCheck.js")
 var registrationToken = "do3dy_XfRbSg0oPjJXxKqO:APA91bEHsnPBlhVeAaxVTHKUGr7snS8NeB5CjrGTg4h412gdwINLyV9l6_k89PDfqH6J8Yed0VKe8--fA-xchn1GdDb3_-Iu6GtttFeESrl8XSG_LfcxJy96GDJ06DGSXBv8G6NWsZrK";
 
-// Connect to firebase server to push new messages to recipient
+//Connect to firebase server to push new messages to recipient
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://communitylink-1d665.firebaseio.com"
@@ -18,15 +18,9 @@ admin.initializeApp({
 module.exports.getMessages = function(req, res) {
   console.log("In chat server");
  
-  // This is just a placeholder for debugging
-  //*****************************************
-  var user1 = "b";
-  var user2 = "c";
-  var newest = "2020-10-10 12:30:45";
-  //*****************************************
-
   var queryString = req.query;
-
+  check.checkMessageQuery(queryString);
+  console.log(queryString)
   db.get(queryString.user1, queryString.user2, queryString.newest, (messages) => {
 
     res.json(JSON.parse(messages));
@@ -36,25 +30,14 @@ module.exports.getMessages = function(req, res) {
 
 module.exports.addMessage = function(req, res) {
   console.log("In chat server");
-
-  // This is just a placeholder for debugging
-  //*****************************************
-  var message = {
-    sender: "c",
-    recipient: "b",
-    time: "2020-10-13 12:25:56",
-    content: "Hi!!!"
-  };
-  //*****************************************
   
   var body = req.body;
 
   if (typeof(body) === "string") {
     body = JSON.parse(body);
   }
-
+  check.checkMessage(body);
   message = body;
-
   var payload = {
       data: message
   };

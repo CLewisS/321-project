298a11b08b8bca60e623f83a113f19f1d572cdd7
EastEden <alexis10cap@gmail.com>
2020-11-15 16:24:56 +0800

    Tests Adding

---
+++ client/app/src/androidTest/java/com/example/community_link/AddServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.community_link;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@LargeTest
+public class AddServiceTest {
+    @Rule
+    public ActivityScenarioRule<AddServiceActivity> activityRule =
+            new ActivityScenarioRule<>(AddServiceActivity.class);
+
+    @Test
+    public void A_CheckExistence(){
+        onView(withId(R.id.etProjectName)).check(matches(isDisplayed()));
+        onView(withId(R.id.type_spinner)).check(matches(isDisplayed()));
+        onView(withId(R.id.etDesc)).check(matches(isDisplayed()));
+        onView(withId(R.id.datePicker)).check(matches(isDisplayed()));
+        onView(withId(R.id.cancelButt)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void B_NotEnoughInformation(){
+        onView(withId(R.id.addServiceButt)).perform(click());
+        onView(withId(R.id.titleErr)).check(matches(isDisplayed()));
+        onView(withId(R.id.descriptionErr)).check(matches(isDisplayed()));
+        onView(withId(R.id.timeErr)).check(matches(isDisplayed()));
+    }
+}
